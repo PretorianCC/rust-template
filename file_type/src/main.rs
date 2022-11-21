8aa9861a6308402e8bf895b62beeeca148d9c891
PretorianCC <pi_2002@mail.ru>
2022-11-21 10:54:09 +0600

    std::fs::File

--- file_type/src/main.rs
+++ file_type/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let file_type = metadata.file_type();
     println!("is_file {:?}", file_type.is_file()); // is_file true
 
-    // Это ссылка?
+    // Это символьная ссылка?
     let metadata = fs::metadata("main.rs").unwrap();
     let file_type = metadata.file_type();
     println!("is_symlink {:?}", file_type.is_symlink()); // is_symlink false
